092a0bca453e0e8f0445aae39bab6bad65b8f9a2
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-08 18:15:18 +0100

    added product data

---
+++ src/components/data/productData.js
@@ -0,0 +1,46 @@
+const productData = [
+    {
+        id: 1,
+        name: "Nike Air Force 1",
+        price: 79.99,
+        description: "The radiance lives on in the Nike Air Force 1 '07, the b-ball icon that puts a fresh spin on what you know best: crisp leather, bold colours and the perfect amount of flash to make you shine.",
+        img: "https://cdn.shopify.com/s/files/1/0013/8094/1893/products/nike-air-force-1-gs-shoes-nike-6_300x300.jpg?v=1610314297"
+    },
+    {
+        id: 2,
+        name: "Nike Air Jordan 1",
+        price: 119.99,
+        description: "Step out in a court classic with these men's Air Jordan 1 Mid trainers. Made with a full-grain leather upper for premium durability, a perforated toe box keeps your feet cool and ventilated.",
+        img: "https://www.thedropdate.com/wp-content/uploads/2019/05/Nike_Air_Jordan_1_Hi_OG_Defiant_Light_Bone_RP-300x300.jpg"
+    },
+    {
+        id: 3,
+        name: "Nike Air Max 95",
+        price: 134.99,
+        description: "The '95 icon gets an essential upgrade with these men's Air Max 95 trainers from Nike. These legendary kicks keep the OG design lines with a leather and synthetic upper. They're inspired by human anatomy and feature a tonal lace up fastening, wavy overlays and a soft foam midsole.",
+        img: "https://thedropdate-media.s3.amazonaws.com/uploads/2020/04/Nike_Air_Max_95_QS_Greedy_2.0_RP-300x300.jpg"
+    },
+    {
+        id: 4,
+        name: "Adidas Yeezy Boost 700",
+        price: 299.99,
+        description: "First releasing in November of 2017, the Yeezy Boost 700’s represented what was a significant shift in Kanye’s design aesthetic, moving from the minimalistic silhouettes of early Yeezy seasons to this chunky runner model.",
+        img: "https://www.moshose.com/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/3/8/38054697_1.jpg"
+    },
+    {
+        id: 5,
+        name: "Adidas Originals Superstar",
+        price: 59.99,
+        description: "Young at heart. Back when they launched in the '70s, adidas Superstar Shoes represented the passion and power of young people. Flash forward to today and that same energy still rings true every time you lace up.",
+        img: "https://thedropdate-media.s3.amazonaws.com/uploads/2020/09/Adidas-Originals-Swarovski-SUPERSTAR-FX7480-300x300.jpg"
+    },
+    {
+        id: 6,
+        name: "Nike Air Jordan 4",
+        price: 199.99,
+        description: "Jordan Brand paid homage to MJ’s alma mater with the Air Jordan 4 University Blue. The University Blue colorway draws a close resemblance to the extremely rare Air Jordan 4 UNC PE that was given to Tarheel student-athletes in 2019.",
+        img: "https://www.thedropdate.com/wp-content/uploads/2018/06/Nike-x-Travis-Scott-Air-Jordan-4-rp-300x300.jpg"
+    }
+]
+
+export default productData
