9ef623db36f9634194a1b7aa4c11d0a2964f47c2
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-08 16:37:09 +0100

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
     <div className="App">
-      Hello
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div id="navbar">
+            <h2>Logo</h2>
+            <ul id="linkList">
+                <li>Home</li>
+                <li>Store</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
