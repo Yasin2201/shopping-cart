c3e7e079210f6c0389b16bf1a212233f4f07bdf1
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-09 17:31:26 +0100

    refcator

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,10 +1,27 @@
-import Products from "./Products"
+import productData from './data/productData'
+import "./Store.css"
+
 
 const Store = () => {
+
+    const products = productData
+
     return (
         <div>
             <h1>Store</h1>
-            <Products />
+            <div id="storeDisplay">
+                {products.map((item) => {
+                    return (
+                        <div key={item.id} className="product">
+                            <img src={item.img} alt={item.name} />
+                            <h2>{item.name}</h2>
+                            <button>Add to cart</button>
+                            <h3>£{item.price}</h3>
+                            <p>{item.description}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
