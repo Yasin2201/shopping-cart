291a49d892276d6a71ca003e8ab3878c65526c97
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-10 02:04:05 +0100

    render itemCart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,24 @@
-const Cart = () => {
+import './Cart.css'
+
+const Cart = ({ itemCart }) => {
     return (
-        <div>
+        <div >
             <h1>Cart</h1>
+            <h2>Total:</h2>
+            <div className="items">
+                {itemCart.map((item) => {
+                    return (
+                        <div key={item.id} className="itemCard">
+                            <img src={item.img} alt={item.name} className="productImage" />
+                            <h2>{item.name}</h2>
+                            <h3>£{item.price}</h3>
+                            <button>-</button>
+                            <input placeholder="qty" />
+                            <button>+</button>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
