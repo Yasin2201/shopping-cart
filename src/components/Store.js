8b4bc5e54c15b59f1f32178e2dea20a1d03c2603
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-10 02:03:38 +0100

    add items to itemCart State

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,10 +1,7 @@
-import productData from './data/productData'
 import "./Store.css"
 
 
-const Store = () => {
-
-    const products = productData
+const Store = ({ products, addToCart }) => {
 
     return (
         <div>
@@ -15,7 +12,7 @@ const Store = () => {
                         <div key={item.id} className="product">
                             <img src={item.img} alt={item.name} />
                             <h2>{item.name}</h2>
-                            <button>Add to cart</button>
+                            <button value={item.id} onClick={addToCart}>Add to cart</button>
                             <h3>£{item.price}</h3>
                             <p>{item.description}</p>
                         </div>
