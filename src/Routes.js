6f6150b040d63b04bff4ab02e2d45265f485803a
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-10 02:02:41 +0100

    added itemCart state and addToCart func

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Store from "./components/Store"
 import Cart from "./components/Cart"
+import productData from './components/data/productData'
 
 
 const Routes = () => {
+    const products = productData
+
+    const [itemCart, setItemCart] = useState([])
+
+    const addToCart = (e) => {
+        const targetValue = parseInt(e.target.value)
+        const foundItem = products.find((item) => { return item.id === targetValue })
+
+        if (!itemCart.includes(foundItem)) {
+            setItemCart([...itemCart, foundItem])
+        }
+        console.log(itemCart)
+    }
+
     return (
         <div>
             <BrowserRouter>
                 <Navbar />
                 <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/store" component={Store} />
-                    <Route exact path="/cart" component={Cart} />
+                    <Route exact path="/" >
+                        <Home />
+                    </Route>
+                    <Route exact path="/store">
+                        <Store products={products} addToCart={addToCart} />
+                    </Route>
+                    <Route exact path="/cart" >
+                        <Cart itemCart={itemCart} />
+                    </Route>
                 </Switch>
             </BrowserRouter>
         </div>
